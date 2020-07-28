f330951fbc8a90002b3cbdbd192d51afa9595c07
Rodrigo <nicolascuevasing@hotmail.com>
2020-07-28 02:04:00 -0300

    correcion con el profe

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,31 +3,33 @@ import './styles/Badges.css';
 import astronauts from '../images/astronauts.svg';
 import conflogo from '../images/platziconf-logo.svg';
 import { Link } from 'react-router-dom';
+import './styles/Home.css';
 
 class HomePages extends React.Component {
     state = {  }
     render() { 
         return ( 
             <React.Fragment>
-
-                        <div className="Badges">
-                            <div className="Badges__hero">
-                                <div className="Badges__container">
+                        <div className="Home">
+                            
+                                <div className="container">
                                     <div className="row">
-                                        <div className="col-3">
-                                            <img className="" src={conflogo} alt="Conf Logo" />
+                                        <div className="Home__col col-12 col-md-4">
+                                            <img className="img-fluid mb-2" 
+                                            src={conflogo} 
+                                            alt="Conf Logo" />
                                             <h1>PRINT YOUT BADGES</h1>
                                             <h3>The easiest way to manage your</h3>
                                             <h3>conference</h3>
                                             <Link to="/badges" className="btn btn-primary">Start Now</Link>
                                         </div>
-                                        <div className="col-9">
-                                        <img className="Badges_conf-logo" src={astronauts} alt="Conf Logo" />
+                                        <div className="Home__col d-none d-md-block col-md-8">
+                                        <img className="img-fluid p-4" src={astronauts} alt="Conf Logo" />
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        
 
             </React.Fragment>
          );
